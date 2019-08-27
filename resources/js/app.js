ad5dd39957265c9dfec688fec45c86698b5e8c9e
Yayang Fauzi R <yayang.rahmatullah@wgs.co.id.com>
2019-08-27 16:42:01 +0700

    tambah fitur outlet, courier, product

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+//require('./bootstrap');
 
-window.Vue = require('vue');
+//window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +27,28 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+// const app = new Vue({
+//     el: '#app',
+// });
+
+import Vue from 'vue'
+import router from './router.js'
+import store from './store.js'
+import App from './App.vue'
+
+new Vue({
+	el: '#dw',
+	router,
+	store,
+	components: {
+		App
+	}
+})
+
+import BootstrapVue from 'bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2'
+
+Vue.use(VueSweetalert2)
+Vue.use(BootstrapVue)
+
+import 'bootstrap-vue/dist/bootstrap-vue.css'
